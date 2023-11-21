a755ad8eadcb7ab346416e2df908a0005c72acc0
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2023-11-21 16:05:19 -0500

    Test factura

--- co.edu.uniquindio.parcial3/src/test/java/co/edu/uniquindio/parcial3/CafeteriaTest.java
+++ co.edu.uniquindio.parcial3/src/test/java/co/edu/uniquindio/parcial3/CafeteriaTest.java
@@ -3,16 +3,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import co.edu.uniquindio.parcial3.dto.CafeteriaDto;
 import co.edu.uniquindio.parcial3.model.Cafeteria;
+import co.edu.uniquindio.parcial3.model.Factura;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class CafeteriaTest {
     CafeteriaDto cafeteriaDto;
     private Cafeteria cafeteriaDto1;
+    private Factura facturaDto;
 
     @BeforeEach
     void init(){
         cafeteriaDto = new CafeteriaDto("Coffe");
         cafeteriaDto1 = new co.edu.uniquindio.parcial3.model.Cafeteria();
+        facturaDto = new Factura();
     }
 
     @Test
@@ -20,5 +23,12 @@ public class CafeteriaTest {
         String esperado = "Caffe";
         String real = cafeteriaDto1.getNombre();
         assertEquals(esperado, real);
+    }
+
+    @Test
+    void verificarTotalFactura(){
+        double esperado = 7500.0;
+        double real = facturaDto.getTotalFactura();
+        assertTrue(real > esperado);
     }
 }
