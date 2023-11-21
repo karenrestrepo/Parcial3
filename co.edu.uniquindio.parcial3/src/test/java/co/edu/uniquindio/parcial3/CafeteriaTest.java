97e26dd0639ee55dd25f8882057b1fee68fbbff8
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2023-11-21 15:18:48 -0500

    Test cafetería

--- co.edu.uniquindio.parcial3/src/test/java/co/edu/uniquindio/parcial3/CafeteriaTest.java
+++ co.edu.uniquindio.parcial3/src/test/java/co/edu/uniquindio/parcial3/CafeteriaTest.java
@@ -1,13 +1,22 @@
 package co.edu.uniquindio.parcial3;
 import static org.junit.jupiter.api.Assertions.*;
 
+import co.edu.uniquindio.parcial3.dto.CafeteriaDto;
 import co.edu.uniquindio.parcial3.model.Cafeteria;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class CafeteriaTest {
+    CafeteriaDto cafeteriaDto;
+
+    @BeforeEach
+    void init(){
+        cafeteriaDto = new CafeteriaDto("Coffe");
+    }
+
     @Test
     void verificarNombre(){
         String esperado = "Caffe";
-        String real = cafeteria.getNombre();
+        String real = cafeteriaDto.nombre();
         assertEquals(esperado, real);
     }
 }
